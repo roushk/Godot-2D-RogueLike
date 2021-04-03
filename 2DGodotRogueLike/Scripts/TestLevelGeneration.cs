613b1a5a7168b12110eeb2f0dab1d17c07f2ea26
roushk <34784335+roushk@users.noreply.github.com>
2021-04-03 16:08:05 -0700

    Brought out the CCLGen into its own class
    Looking around to figure out the best way to fix the bug where the parents aren't set quite properly

--- 2DGodotRogueLike/Scripts/TestLevelGeneration.cs
+++ 2DGodotRogueLike/Scripts/TestLevelGeneration.cs
@@ -3,17 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
-
-
 public class TestLevelGeneration : Node2D
 {
 
+	public CCLGenerator CCLGen = new CCLGenerator();
 	//Signals
 	public void GenerateCompleteMapButton_Callback()
 	{
 		GenerateMap(maxIterations, true);
-		ContiguousCaveSort();
+		CCLGen.UpdateMap(width, height, ref terrainMap);
+		CCLGen.CCLAlgorithm();
 		UpdateMapData();
 	}
 
@@ -22,6 +21,7 @@ public class TestLevelGeneration : Node2D
 	{
 		GD.Print("Clicked Generate New Tile Map Button");
 		GenerateMap(maxIterations, true);
+		CCLGen.UpdateMap(width, height, ref terrainMap);
 		UpdateMapData();
 
 	}
@@ -31,6 +31,7 @@ public class TestLevelGeneration : Node2D
 	{
 		GD.Print("Clicked Prune Tile Map Button");
 		GenerateMap(1, false);
+		CCLGen.UpdateMap(width, height, ref terrainMap);
 		UpdateMapData();
 	}
 
@@ -39,14 +40,26 @@ public class TestLevelGeneration : Node2D
 	public void GenerateCaveGroups_Callback()
 	{
 		GD.Print("Generate Cave Groups Button");
-		ContiguousCaveSort();
+		CCLGen.CCLAlgorithm();
 		UpdateMapData();
 	}
 
-	  public void ClearMapButton_Callback()
+	public void ClearMapButton_Callback()
   {
 		GD.Print("Clicked Clear Map Button");
 		ClearMap();
+	}
+
+	public void ViewRootGroups_Callback()
+	{
+		GD.Print("Clicked ViewRootGroups_Callback Button");
+		CCLGen.VisualizeIDTree(CCLGenerator.VisualizeMode.Root);
+	}
+
+	public void ViewIndividualGroups_Callback()
+	{
+		GD.Print("Clicked ViewRootGroups_Callback Button");
+		CCLGen.VisualizeIDTree(CCLGenerator.VisualizeMode.Individual);
 	}
 
 	public PackedScene IDColorMapScene = ResourceLoader.Load<PackedScene>("res://TemplateScenes/IDAndColorUIElement.tscn");
@@ -65,7 +78,7 @@ public class TestLevelGeneration : Node2D
   //declare Foreground and Background map variables
   public Godot.TileMap ForegroundMap;
 
-  public Godot.TileMap FloodFillMap; 
+  public Godot.TileMap mapIDVisualizationRef; 
 
   //range of 0 to 100 with step range of 5
   [Export(PropertyHint.Range,"0,100,1")]
@@ -123,18 +136,23 @@ public class TestLevelGeneration : Node2D
 		{
 			for(int j = -1; j <= 1; ++j)  
 			{
-			if(i == 0 && j == 0)
-				continue;
+				if(i == 0 && j == 0)
+					continue;
 
-			neighborsToCheck[pos++] = new Vector2(i, j);
-			}
+					neighborsToCheck[pos++] = new Vector2(i, j);
+				}
 		}
 
 		ForegroundMap = GetNode("ForegroundMap") as TileMap;
-		FloodFillMap = GetNode("FloodFillMap") as TileMap;
+		mapIDVisualizationRef = GetNode("FloodFillMap") as TileMap;
 
 		GenerateMap(maxIterations, true);
-		ContiguousCaveSort();
+
+		CCLGen.SetVisualizationMap(ref mapIDVisualizationRef);
+		
+		CCLGen.UpdateMap(width, height, ref terrainMap);
+
+		CCLGen.CCLAlgorithm();
 		UpdateMapData();
   }
 
@@ -148,7 +166,7 @@ public class TestLevelGeneration : Node2D
   public void ClearMap()
   {
 		ForegroundMap.Clear();
-		FloodFillMap.Clear();
+		CCLGen.Clear();
 		terrainMap = null;
   }
 
@@ -207,204 +225,6 @@ public class TestLevelGeneration : Node2D
 				}
 			}
 		}
-  }
-
-  //runs simulation
-  public void ContiguousCaveSort()
-  {
-		//Going from top left to bottom right
-		//from an arbitary pixel p
-		//get the pixel to the left and to the top of p
-		// ? X ? 
-		// X p ?
-		// ? ? ?
-		//loop over terrain map
-		
-		//This long line is a dictionary from Id's to a hashset (unique list) of integer x,y coords as a keypair
-		Dictionary<KeyValuePair<int, int>, int> dictIDToListofCoords = new Dictionary<KeyValuePair<int, int>, int>();
-
-		//dictionary of int ID of set to ID of parent set 
-		//key = ID of node, value = ID of parent
-		Dictionary<int,int> IDToParent = new Dictionary<int, int>();
-
-		const int maxNumGroups = 1000;
-		//ID to size for the first iteration
-		int [] sizeOfPixelGroup = new int [maxNumGroups];
-
-		//********************************************************************//
-		//Connected Component Labeling
-		//First Pass
-		//starting ID is 1 so any with id zero have no parent
-		int nextNewPixelID = 1;
-
-		for(int y = 0; y < height; ++y)
-		{
-			for(int x = 0; x < width; ++x)
-			{  
-				//ignore walls
-				if(terrainMap[x,y] != 1)
-					continue;
-
-
-				//We want to check if there is a pixel to the left and above the current pixel that we are checking	
-				int leftPixelID = 0;
-				int abovePixelID = 0;
-				
-				//get the pixel to the left and to the top of p
-				// ? X ? 
-				// X p ?
-				// ? ? ?
-				
-				bool leftPixelExists = dictIDToListofCoords.TryGetValue(new KeyValuePair<int, int>(x - 1, y), out leftPixelID);
-				bool abovePixelExists = dictIDToListofCoords.TryGetValue(new KeyValuePair<int, int>(x, y - 1), out abovePixelID);
-				
-				int leftPixelCurrentParentID = 0;
-				bool leftHasParent = IDToParent.TryGetValue(leftPixelID, out leftPixelCurrentParentID);
-				
-				int abovePixelCurrentParentID = 0;
-				bool aboveHasParent = IDToParent.TryGetValue(abovePixelID, out abovePixelCurrentParentID);
-
-				if(leftPixelExists && abovePixelExists)
-				{
-					//Parent is always smaller than child
-					if(leftPixelID < abovePixelID)
-					{
-						//it doesnt have the parent or it has a parent and the left pixel id is less than the above pixel id
-						if(!aboveHasParent || (aboveHasParent && leftPixelID < abovePixelCurrentParentID))
-						{
-							//set the id to the left pixel
-							IDToParent[abovePixelID] = leftPixelID;
-						}
-					}
-					else if(leftPixelID > abovePixelID)
-					{
-						if(!leftHasParent || (leftHasParent && abovePixelID < leftPixelCurrentParentID))
-						{
-							IDToParent[leftPixelID] = abovePixelID;
-						}
-					}
-					//else dont set same ID to child of itself
-
-					//leftPixelID size > abovePixelID size
-					if(sizeOfPixelGroup[leftPixelID] > sizeOfPixelGroup[abovePixelID])
-					{
-						//Add to the larger set
-						sizeOfPixelGroup[leftPixelID]++;
-						dictIDToListofCoords[new KeyValuePair<int, int>(x,y)] = leftPixelID;
-					}
-					else //leftPixelID size <= abovePixelID size
-					{
-						//Add to either set
-						sizeOfPixelGroup[abovePixelID]++;
-						dictIDToListofCoords[new KeyValuePair<int, int>(x,y)] = abovePixelID;
-					}
-				}
-				//now we decide to add to leftPixelID first if possible
-				else if(leftPixelExists && !abovePixelExists) //redundant !abovePixelExists for clarity
-				{
-					sizeOfPixelGroup[leftPixelID]++;
-					//if left key than set to left key
-					dictIDToListofCoords[new KeyValuePair<int, int>(x,y)] = leftPixelID;
-				}
-				//If cannot get left key than check up key
-				else if(abovePixelExists && !leftPixelExists)  //redundant !leftPixelExists for clarity
-				{
-					sizeOfPixelGroup[abovePixelID]++;
-					//if up key than set to up key
-					dictIDToListofCoords[new KeyValuePair<int, int>(x,y)] = abovePixelID;
-				}
-				//if cannot get left OR up key than create new key
-				else
-				{
-					dictIDToListofCoords[new KeyValuePair<int, int>(x,y)] = nextNewPixelID++;
-					sizeOfPixelGroup[nextNewPixelID] = 1;
-				}
-			}
-		}
-		
-		Console.WriteLine("Max ID = " +  nextNewPixelID.ToString());
-
-		//Print a tree of each current ID
-		for(int i = 0; i < nextNewPixelID; i++)
-		{
-			//Print the parent path
-			int initialID = i;
-			int ID = i;
-			int IDout = 0;
-			//while we can get parents follow the path
-			//Get Root
-			string treeRoot = "Following Tree " + ID.ToString();
-			while(IDToParent.TryGetValue(ID, out IDout))
-			{
-				//Console.WriteLine(" ->" + IDout.ToString());
-				treeRoot += " -> " + IDout.ToString();
-				//follow tree up
-				ID = IDout;
-			}
-
-			//Node newNode = IDColorMapScene.Instance();
-			//MapGenColorListNode.AddChild(newNode);
-			//newNode.Owner = MapGenColorListNode;
-
-			//(newNode.FindNode("Label", true, true) as Label).Text = "ID: " + ID;
-			//(newNode.FindNode("ColorRect", true, true) as ColorRect).Color = ;
-
-			GD.Print(treeRoot);
-		}
-
-		//put all the pixels into this new map so that we have the links between each pixel and each set still
-		Dictionary<KeyValuePair<int, int>, int> listOfCoords = new Dictionary<KeyValuePair<int, int>, int>();
-
-		//for every pixel
-		for(int i = 0; i < dictIDToListofCoords.Count; ++i)
-		{
-			
-			var element = dictIDToListofCoords.ElementAt(i);
-			var key = element.Key;
-			var value = element.Value;
-
-			int initialID = element.Value;
-			int ID = element.Value;
-			int IDout = 0;
-			//while we can get parents follow the path
-			//Get Root
-			while(IDToParent.TryGetValue(ID, out IDout))
-			{
-				//follow tree up
-				ID = IDout;
-			}
-			
-			//move this 1 pixel to its parent size
-			sizeOfPixelGroup[ID]++;
-			sizeOfPixelGroup[initialID]--;
-
-			//add the sets together
-			listOfCoords.Add(key,ID);
-			//here when this set has no parent
-			
-			FloodFillMap.SetCell(-key.Key + width / 2, -key.Value + width / 2, ID);
-			
-		}
-
-		for(int i = 0; i < maxNumGroups; ++i)
-		{
-			if(sizeOfPixelGroup[i] > 0)
-			{
-				Console.WriteLine("Count of pixels with ID " + i.ToString() + " Is " + sizeOfPixelGroup[i].ToString());
-			}
-		}
-
-		//set the flood fill map according to the data 
-		/*
-		for(int i = 0; i < dictIDToListofCoords.Count; ++i)
-		{
-			var element = dictIDToListofCoords.ElementAt(i);
-			var key = element.Key;
-			var value = element.Value;
-			//FloodFillMap.SetCell(-key.Key + width / 2, -key.Value + width / 2, value);
-			
-		}
-		*/
   }
 
 	private void UpdateMapData()
